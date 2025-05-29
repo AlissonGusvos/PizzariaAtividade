3cb74462904528b89958e2b9fbc0155c2f75b5b5
AlissonGusvos <gustavoalisson322@gmail.com>
2025-05-28 22:28:58 -0300

    pacote de configuração adicionado

--- src/main/java/com/pizzariaAtividade/Curso/Spring/pizza/PizzaDTO.java
+++ src/main/java/com/pizzariaAtividade/Curso/Spring/pizza/PizzaDTO.java
@@ -1,4 +1,14 @@
 package com.pizzariaAtividade.Curso.Spring.pizza;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PizzaDTO {
+    private String nome;
+    private double preco;
+    private boolean disponivel;
+    private Sabor sabor;
+    private Tamanho tamanho;
 }
---
+++ src/main/java/com/pizzariaAtividade/Curso/Spring/pizza/PizzaService.java
@@ -0,0 +1,15 @@
+package com.pizzariaAtividade.Curso.Spring.pizza;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PizzaService {
+
+    private final PizzaRepository pizzaRepository;
+
+    private final ModelMapper modelMapper;
+
+}
